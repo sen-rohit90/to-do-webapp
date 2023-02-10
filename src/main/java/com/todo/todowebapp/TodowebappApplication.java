dc3ecb91d88fee30d08dbf061f1726f44543b0bd
sen-rohit90 <sen.rohit65@gmail.com>
2023-02-10 10:33:46 +0530

    Initial setup in Eclipse

---
+++ src/main/java/com/todo/todowebapp/TodowebappApplication.java
@@ -0,0 +1,13 @@
+package com.todo.todowebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodowebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodowebappApplication.class, args);
+	}
+
+}
